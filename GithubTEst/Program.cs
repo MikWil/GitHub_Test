107ab1cfa96afeadb14cb0a9b48c288a40a55358
MikWil <mikaelwilkon@gmail.com>
2013-09-16 14:47:44 +0200

    I made some YOLO changes for dis application

---
+++ GithubTEst/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTEst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Murat = "DisGreek"; 
+            string Retard = Murat;
+            int Elo = 599;
+           
+            for (int Elonum = 0; Elo < 600; Elonum++)
+            {
+
+
+                if (Murat == Retard && Elo < 600)
+                {
+                    Console.WriteLine(Murat + "ALLAHUAKBAR");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ GithubTEst/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GithubTEst")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("GithubTEst")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d53f5a5-becc-441c-a49f-dba82c775bc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
